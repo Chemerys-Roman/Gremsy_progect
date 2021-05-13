afda49303f2d59384102b71f9c77752c3122c79b
Vladislav Maksimchuk <vladmv@gremcy.com>
2021-05-13 20:46:08 +0300

    wip

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,7 +3,6 @@
 import gulp from 'gulp';
 import Path from 'path';
 import sass from 'gulp-sass';
-import log from 'fancy-log';
 import autoprefixer from 'gulp-autoprefixer';
 import sourcemaps from 'gulp-sourcemaps';
 import connect from 'gulp-connect';
@@ -13,18 +12,16 @@ import babel from 'gulp-babel';
 import imagemin from 'gulp-imagemin';
 import pngquant from 'imagemin-pngquant';
 import mozjpeg from 'imagemin-mozjpeg';
-import util from 'gulp-util';
-import webpack from 'webpack';
-import webpackStream from 'webpack-stream';
 import nunjucks from 'gulp-nunjucks';
 import inject from 'gulp-inject-string';
+import log from 'fancy-log';
+import favicons from 'gulp-favicons';
 
 const svgmin   = require('gulp-svgmin');
 const changed  = require('gulp-changed');
 const del      = require('del');
-const prettify = require('gulp-html-prettify');
 
-var production = process.env.NODE_ENV === 'production';
+const production = process.env.NODE_ENV === 'production';
 
 gulp.task('styles', () => {
     return gulp.src(['./src/styles/*.scss'])
@@ -46,24 +43,39 @@ gulp.task('html', () => {
     return gulp.src('./src/html/**/[^_]*.html')
         // .pipe(data(() => ({data: getData()})))
         .pipe(nunjucks.compile({}, { trimBlocks: true, lstripBlocks: true }))
-        .pipe(prettify({
-          indent_size: 4,
-          indent_char: ' '
-        }))
         .pipe(inject.after('src="/js/main.js', '?' + timestamp))
         .pipe(inject.after('href="/styles/main.css', '?' + timestamp))
         .pipe(gulp.dest('dist'))
         .pipe(connect.reload());
 });
 
+gulp.task('favicons', () => {
+    return gulp.src('./src/img/favicon/*.{jpg,jpeg,png,gif}')
+        .pipe(favicons({
+            icons: {
+                appleIcon: true,
+                favicons: true,
+                online: false,
+                appleStartup: false,
+                android: false,
+                firefox: false,
+                yandex: false,
+                windows: false,
+                coast: false
+            }
+        }))
+        .pipe(gulp.dest('./dist/images/favicons/'));
+});
+
 gulp.task('watch', function(done) {
     gulp.watch('./src/styles/**/*.scss', gulp.series('styles'));
     gulp.watch('./src/html/**/*.html', gulp.series('html'));
-    gulp.watch('./src/js/**/*.js', gulp.series('webpack'));
+    gulp.watch('./src/img/favicon/**/*', gulp.series('favicons'));
+    gulp.watch('./src/js/**/*.js', gulp.series('js'));
     gulp.watch('./static/images/**/*.{jpg,png,jpeg,svg,gif}', gulp.series('copy'));
     gulp.watch('./static/fonts/**/*.{ttf,eot,woff,woff2}', gulp.series('copy'));
     gulp.watch('./static/lib/**/*', gulp.series('copy'));
-    
+
     done();
 });
 
@@ -77,18 +89,12 @@ gulp.task('serve', function(done) {
     done();
 });
 
-const webpackConfig = require('./webpack.config.js');
-webpackConfig.mode = production ? 'production' : 'development';
-webpackConfig.optimization.minimize = production ? true : false;
-
-gulp.task('webpack', function() {
-    return gulp.src('./src/js/main.js')
-                .pipe(webpackStream(webpackConfig))
-                .pipe(gulp.dest('./dist/js/'))
-                .pipe(connect.reload());
+gulp.task('js', function() {
+    return gulp.src('./src/js/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('./dist/js/'))
+        .pipe(connect.reload())
 });
-
-
 
 gulp.task('svg', function() {
     return gulp
@@ -142,14 +148,15 @@ gulp.task('copy:watch', function() {
 
 gulp.task('clean', function(cb) {
     return del([
-        './dist/'
+        './dist/*'
     ]).then(function(paths) {
-        util.log('Deleted:', util.colors.magenta(paths.join('\n')));
+        log('Deleted: ' + paths.join('\n'));
     });
 });
 
 gulp.task('copy', gulp.series('copy:img', 'copy:fonts', 'copy:root'));
-gulp.task('build', gulp.series('clean', 'copy', 'html', 'styles', 'webpack'));
+gulp.task('build', gulp.series('clean', 'copy', 'favicons', 'html', 'styles', 'js'));
 
-const defaultTasks = gulp.parallel('html', 'styles', 'serve', 'webpack', 'watch')
+const defaultTasks = gulp.parallel('html', 'styles', 'serve', 'js', 'watch')
+
 export default defaultTasks
